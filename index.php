d118bb4f6ab22b9367c80840ab53d668f0d0f679
dblancoformacion <dblancoformacion@gmail.com>
2019-06-06 16:05:17 +0200

    Session_destroy()

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php
 session_start();
-
-if(!isset($_SESSION)) echo '
-	<form>
-		<input name="login" placeholder="login" autofocus />
-		<input name="password" placeholder="password" type="password"/>
-		<button>Acceder</button>
-	</form>
-';
-else echo ' <a href="?exit=1">Cerrar</a> ';
+if(isset($_GET['salir'])){
+	session_destroy();
+	session_start();
+}
 
 if( isset($_GET['login']) && isset($_GET['password']) ){
 	if( $_GET['login']=='david'
 		&& $_GET['password']=='hola'
 	){
-		echo 'Hola '.$_GET['login'].', tienes acceso';
 		$_SESSION['login']='david';
 		
 	}
@@ -23,6 +17,15 @@ if( isset($_GET['login']) && isset($_GET['password']) ){
 		session_destroy();
 	}
 }
+
+if(!isset($_SESSION['login'])) echo '
+	<form>
+		<input name="login" placeholder="login" autofocus />
+		<input name="password" placeholder="password" type="password"/>
+		<button>Acceder</button>
+	</form>
+';
+else echo ' <a href="?salir=1">Cerrar</a> ';
 
 echo '<pre>';
 print_r($_SESSION);
