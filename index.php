a38b9647e879105a1e3a3a1668087c93f8c229de
dblancoformacion <dblancoformacion@gmail.com>
2019-06-06 15:30:23 +0200

    Versión preliminar

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+echo '
+	<form>
+		<input name="login" placeholder="login" autofocus />
+		<input name="password" placeholder="password" type="password"/>
+		<button>Acceder</button>
+	</form>
+';
+
+if( isset($_GET['login']) && isset($_GET['password'])
+		&& $_GET['login']=='david'
+		&& $_GET['password']=='hola'
+){
+	echo 'Hola '.$_GET['login'].', tienes acceso';
+	$_SESSION['login']='david';
+	echo '<a href="?exit=1">Cerrar</a>';
+}
+else{
+	echo 'Acceso no autorizado, introduce tus credenciales';
+	session_destroy();
+}
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
