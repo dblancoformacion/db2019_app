a71aaeaa857f982cd0b860dc4eed664df5aecbfb
dblancoformacion <dblancoformacion@gmail.com>
2019-06-06 15:51:40 +0200

    Oculto el formulario si estoy logueado

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 session_start();
 
-echo '
+if(!isset($_SESSION)) echo '
 	<form>
 		<input name="login" placeholder="login" autofocus />
 		<input name="password" placeholder="password" type="password"/>
 		<button>Acceder</button>
 	</form>
 ';
+else echo ' <a href="?exit=1">Cerrar</a> ';
 
 if( isset($_GET['login']) && isset($_GET['password']) ){
 	if( $_GET['login']=='david'
@@ -15,7 +16,7 @@ if( isset($_GET['login']) && isset($_GET['password']) ){
 	){
 		echo 'Hola '.$_GET['login'].', tienes acceso';
 		$_SESSION['login']='david';
-		echo ' <a href="?exit=1">Cerrar</a> ';
+		
 	}
 	else{
 		echo 'Acceso no autorizado, introduce tus credenciales';
