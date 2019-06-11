adbd6e10b62cefd83521abcc7d16367349f032ef
dblancoformacion <dblancoformacion@gmail.com>
2019-06-11 16:13:35 +0200

    Borrador de la vista

---
+++ domotica/vista/index.php
@@ -0,0 +1,29 @@
+<div style="
+		display:inline-block;
+		border: solid 1px;
+		border-radius: 10px;
+		text-align: center;
+		padding: 20px;
+	">
+	<div>
+	Aula 7
+	</div>
+	<img src="../imgs/foco_ok.png">
+	<img src="../imgs/foco_ko.png">
+</div>
+
+<div style="
+		display:inline-block;
+		border: solid 1px;
+		border-radius: 10px;
+		text-align: center;
+		padding: 20px;
+	">
+	<div>
+	Aula 8
+	</div>
+	<img src="../imgs/foco_ok.png">
+	<img src="../imgs/foco_ko.png">
+	<img src="../imgs/foco_ko.png">
+	<img src="../imgs/foco_ko.png">
+</div>
