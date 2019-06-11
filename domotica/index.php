30d9b21bbfb425bd015653f678343a1ce0bb074b
dblancoformacion <dblancoformacion@gmail.com>
2019-06-11 17:19:56 +0200

    Vista preliminar

--- domotica/index.php
+++ domotica/index.php
@@ -5,17 +5,48 @@ Class Luminarias{
 		$conn=new mysqli('localhost','root','','db2019');
 		$this->datos = $conn->query("
 			SELECT * FROM luminarias
-				JOIN estancias USING(id_estancia);
+				JOIN estancias USING(id_estancia)
+				ORDER BY estancia,luminaria;
 		")->fetch_all(MYSQLI_ASSOC);
+	}
+	function mostrar(){
+		$txt='<div>';
+		$estancia=null;
+		foreach($this->datos as $luminaria){
+			if($estancia!=$luminaria['estancia']){
+				$estancia=$luminaria['estancia'];
+				$txt.='
+					</div>								
+					<div style="
+							display:inline-block;
+							border: solid 1px;
+							border-radius: 10px;
+							text-align: center;
+							padding: 20px;
+						">
+						<div>
+						'.$estancia.'
+						</div>
+				';
+			}				
+			$txt.= '<img src="imgs/foco_ok.png">';
+		}
+		return $txt.'</div>';
 	}
 }
 
 $luminarias=new Luminarias();
+
+// Modelo
 $luminarias->inicializa();
 
+// Controlador
 
+// Vista
+echo $luminarias->mostrar();
+/*
 echo '<pre>';
 print_r($luminarias->datos);
 echo '</pre>';
-
+*/
 ?>
